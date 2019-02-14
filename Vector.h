ca5c69bd5cb234e163a2ed95236cc284c7bd7ec4
Курбан <kurban_mamaev@mail.ru>
2019-02-14 15:01:39 +0500

    Update Vector.h

--- Vector.h
+++ Vector.h
@@ -1,16 +1,49 @@
 #ifndef OOP_VECTOR_H
 #define OOP_VECTOR_H
 
-class Vector {
-    int *arr;
-    protected:
-        int top;
-        int capacity;
+#include <cstdlib>
+
+
+template <class T> class Vector {
+    T *arr;
+    int capacity;
+    int top;
+
     public:
         Vector(int);
         ~Vector();
         int getLastEl();
         void pushEl(int);
+        T& operator[](int i);
+
 };
+
+template <class T>
+Vector<T>::Vector(int capacity) {
+    this->capacity = capacity;
+    top = 0;
+    arr = (int*) malloc(sizeof(int) * capacity);
+}
+
+template <class T>
+Vector<T>::~Vector() {
+    free(arr);
+}
+
+template <class T>
+void Vector<T>::pushEl(int el) {
+    if(top == capacity - 1)
+        return;
+    arr[top++] = el;
+}
+
+template <class T>
+T& Vector<T>::operator [](int i) {
+    return arr[i];
+}
+
+
+
+
 
 #endif
