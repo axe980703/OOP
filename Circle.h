de7dd80a86d557d03c21a33411654a9d7ed06419
Курбан <kurban_mamaev@mail.ru>
2019-02-19 16:03:57 +0500

    Update Circle.h

--- Circle.h
+++ Circle.h
@@ -1,4 +1,7 @@
+#include <cstdlib>
 #include "Figure.h"
+
+#define INIT_MEM 1024
 
 class Circle : public Figure {
     double radius;
@@ -9,11 +12,21 @@ class Circle : public Figure {
 
         void setRadius(double);
         double getRadius();
+        void* operator new(size_t size);
 
-        class IllegalNumber{
-            public:
-            double value;
-            IllegalNumber(double v) : value(v) {}
-        };
+
+    class MemoryManager {
+        size_t size;
+    public:
+        MemoryManager(size_t sz): size(sz) {
+            allocMem(INIT_MEM);
+        }
+        ~MemoryManager() {
+
+        }
+
+        void* allocMem(size_t sz);
+        void freeMem(void *ptr);
+    };
 };
 
